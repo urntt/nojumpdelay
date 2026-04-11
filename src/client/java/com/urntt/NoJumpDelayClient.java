e4786c154239604c7f99a7f31786d0c3cb02a8fa
urntt <urntts@gmail.com>
2026-04-11 01:30:50 -0700

    air jump

--- src/client/java/com/urntt/NoJumpDelayClient.java
+++ src/client/java/com/urntt/NoJumpDelayClient.java
@@ -1,11 +1,28 @@
 package com.urntt;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import com.urntt.mixin.client.LivingEntityAccessor;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
+import net.minecraft.resources.Identifier;
+import org.lwjgl.glfw.GLFW;
 
 public final class NoJumpDelayClient implements ClientModInitializer {
+	private static final KeyMapping.Category AIR_JUMP_CATEGORY =
+			KeyMapping.Category.register(Identifier.fromNamespaceAndPath("nojumpdelay", "air_jump"));
+
+	private static final KeyMapping AIR_JUMP_MODIFIER = KeyMappingHelper.registerKeyMapping(
+			new KeyMapping(
+					"key.nojumpdelay.air_jump_modifier",
+					InputConstants.Type.KEYSYM,
+					GLFW.GLFW_KEY_R,
+					AIR_JUMP_CATEGORY
+			)
+	);
+
 	@Override
 	public void onInitializeClient() {
 		ClientTickEvents.END_CLIENT_TICK.register(this::onEndClientTick);
@@ -17,5 +34,15 @@ public final class NoJumpDelayClient implements ClientModInitializer {
 		}
 
 		((LivingEntityAccessor) client.player).setNoJumpDelay(0);
+
+		if (client.screen != null) {
+			return;
+		}
+
+		while (client.options.keyJump.consumeClick()) {
+			if (!client.player.onGround() && AIR_JUMP_MODIFIER.isDown()) {
+				client.player.jumpFromGround();
+			}
+		}
 	}
 }
